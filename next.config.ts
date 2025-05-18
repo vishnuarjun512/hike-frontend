5f2b470fa8643a7978aa32fdcf46e49b1fba51d6
Vishnu V V <vishnuarjun512.com>
2025-05-18 22:43:08 +0530

    Build settings changed to export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: "export", // Required for static export
 };
 
 export default nextConfig;
